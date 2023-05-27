dbcd26af36a4bb2fb43e9e512e3a7d2a604d2c3b
Hp <Hp@DESKTOP-LTJOJD3>
2023-05-27 21:24:44 +0300

    Add project files.

---
+++ ödev/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            for (int a = 1; a <= 10; a++)
+            {
+                toplam += a;
+            }
+            Console.WriteLine("Sonuç : " + toplam);
+            Console.ReadKey();
+        }
+    }
+}
